6d2f4731ddaf88ab008b4bdf2c96ad3cd89618c3
Tyler-Wagner <tylerrwagner38@gmail.com>
2022-11-29 12:08:31 -0600

    fixed general search window
    finally got the search and view buttons to work

--- Java Stuff/WindowPackage/searchWindow.java
+++ Java Stuff/WindowPackage/searchWindow.java
@@ -7,9 +7,6 @@ Modified by: Tyler Wagner
  */
 
 package WindowPackage;
-
-
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
@@ -23,7 +20,7 @@ public class searchWindow extends JFrame implements ActionListener {
     JFrame frame;
     JLabel label1, label2, label3;
     JTextField searchBar;
-    JButton b1;
+    JButton b1, b2;
     ResultSet rs, rs1;
     Statement st, st1;
     String ids;
@@ -37,12 +34,15 @@ public class searchWindow extends JFrame implements ActionListener {
         label1 = new JLabel("Fetching Information");
         label2 = new JLabel("Search Title: ");
         b1 = new JButton("Search");
+        b2 = new JButton("View All");
         searchBar = new JTextField();
         label1.setBounds(125, 50, 350, 40);
-        label2.setBounds(50, 110, 75, 20);
-        searchBar.setBounds(125,110,175,20);
+        label2.setBounds(50, 110, 100, 20);
+        searchBar.setBounds(150,110,175,20);
         b1.setBounds(150, 150, 150, 20);
+        b2.setBounds(150,205,150,20);
         b1.addActionListener(this);
+        b2.addActionListener(this);
         setTitle("Fetching Game Info from Database");
         setLayout(null);
         setVisible(true);
@@ -52,6 +52,7 @@ public class searchWindow extends JFrame implements ActionListener {
         add(label2);
         add(searchBar);
         add(b1);
+        add(b2);
 
 
 
@@ -82,7 +83,13 @@ public class searchWindow extends JFrame implements ActionListener {
         {
             showTableData();
         }
+        else if (event.getSource() == b2)
+        {
+            showAll();
+        }
     }
+
+
 
     public void showTableData()
     {
@@ -91,6 +98,7 @@ public class searchWindow extends JFrame implements ActionListener {
         DefaultTableModel model = new DefaultTableModel();
         table = new JTable();
         table.setModel(model);
+        model.setColumnIdentifiers(columnNames);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         table.setFillsViewportHeight(true);
         JScrollPane scroll = new JScrollPane(table);
@@ -129,6 +137,46 @@ public class searchWindow extends JFrame implements ActionListener {
         }catch(Exception e)
         {
             System.out.println("ERROR: " + e);
+        }
+
+        frame.add(scroll);
+        frame.setVisible(true);
+        frame.setSize(500,500);
+    }
+
+    public void showAll()
+    {
+        frame = new JFrame("Database Search Result");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        DefaultTableModel model = new DefaultTableModel();
+        table = new JTable();
+        table.setModel(model);
+        model.setColumnIdentifiers(columnNames);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        table.setFillsViewportHeight(true);
+        JScrollPane scroll = new JScrollPane(table);
+        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        from = (String) searchBar.getText();
+        String gameName = "";
+        String gameSystem = "";
+        String releaseDate = "";
+
+        try{
+            pst = con.prepareStatement("select * from game");
+            ResultSet rs = pst.executeQuery();
+            while(rs.next())
+            {
+                gameName = rs.getString("gameName");
+                gameSystem = rs.getString("gameSystem");
+                releaseDate = rs.getString("releaseDate");
+                model.addRow(new Object[]{gameName, gameSystem, releaseDate});
+            }
+
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
         }
 
         frame.add(scroll);
